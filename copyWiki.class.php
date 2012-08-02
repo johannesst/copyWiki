b74a70c4d55ded17d3b7c55b7ae7105ee63dada1
johannes.stichler <johannes.stichler@d56861a9-e89f-4f46-9006-0833ef3c6828>
2012-08-02 11:37:55 +0000

    Erste UI Version
    git-svn-id: http://sv-svn1.hfwu.int/svn/neocopyWiki@1 d56861a9-e89f-4f46-9006-0833ef3c6828

---
+++ copyWiki.class.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: johannesstichler
+ * Date: 02.08.12
+ * Time: 08:30
+ * To change this template use File | Settings | File Templates.
+ */
+class copyWiki extends StudipPlugin implements SystemPlugin
+{
+    private $vlid, $userright;
+    public function __construct() {
+        global  $user, $perm, $auth;
+        parent::__construct();
+        $perm->studip_perms;
+        if(isset($_REQUEST["auswahl"])) $this->vlid = $_REQUEST["auswahl"];
+        if(isset($_REQUEST["cid"])) $this->vlid = $_REQUEST["cid"];
+        if(isset($this->vlid)) {
+            $userid = $GLOBALS['user']->id;
+            echo $userid;
+            $userrechte =   $perm->studip_perms;
+            $this->userright = $userrechte[$this->vlid][$userid];
+            if($this->userright == "dozent") {
+                $navigation = new AutoNavigation("Wiki kopieren", PluginEngine::getURL($this, array(), "show"));
+                Navigation::addItem('/course/copy', clone $navigation);
+            }
+
+        }
+
+    }
+
+    public function show_action() {
+        $template = $this->getTemplate("copy.php");
+        $this->vlid = $_REQUEST["cid"];
+        $template->set_attribute('infos', $this->getWikiInfos());
+        $template->set_attribute('vls', $this->getUserVl());
+        echo $template->render();
+    }
+
+    private function getWikiInfos() {
+        $db = DBManager::get();
+        $anzahlSeiten = $db->prepare('SELECT count( `range_id` )
+                                FROM `wiki`
+                                WHERE `range_id` LIKE ?');
+        $anzahlSeiten->execute(array($this->vlid));
+
+        $seiten =  $anzahlSeiten->fetchColumn();
+        $infos["seiten"] = $seiten["count(`range_id`)"];
+        $db2 = DBManager::get();
+        $aenderung = $db2->prepare('SELECT *
+                                    FROM wiki
+                                    WHERE range_id LIKE ?
+                                    ORDER BY chdate DESC');
+        $aenderung->execute(array($this->vlid));
+
+        $laenderung =  $aenderung->fetchAll();
+        $infos["aenderung"] = $laenderung[0]["chdate"];
+        return $infos;
+    }
+
+    private function getUserVl() {
+        $db = DBManager::get();
+        $aenderung = $db->prepare("SELECT s.Seminar_id as id, s.Name as name, s.start_time FROM `seminare` as s
+                                    INNER JOIN seminar_user as u ON s.`Seminar_id` = u.Seminar_id
+                                    WHERE u.user_id = ? AND u.status = 'dozent'");
+        $aenderung->execute(array($GLOBALS['user']->id));
+
+        $vls =  $aenderung->fetchAll();
+        print_r($vls);
+       foreach($vls as $v) {
+            $semester = $db->prepare("SELECT s.name, s.vorles_beginn as beginn, s.vorles_ende as ende  FROM `semester_data` as s
+                                      WHERE beginn <= ? AND ende >= ?");
+            $semester->execute(array($v["start_time"],$v["start_time"]));
+
+            $sem =  $semester->fetchAll();
+            $sem = $sem[0];
+
+            $v["sem_name"] = $sem["name"];
+           $vlliste[] = $v;
+       }
+
+        return $vlliste;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    protected function getDisplayName() {
+        return "VL Manager";
+    }
+
+    protected function getTemplate($template_file_name, $layout = "without_infobox") {
+        if ($layout) {
+            if (method_exists($this, "getDisplayName")) {
+                PageLayout::setTitle($this->getDisplayName());
+            } else {
+                PageLayout::setTitle(get_class($this));
+            }
+        }
+        if (!$this->template_factory) {
+            $this->template_factory = new Flexi_TemplateFactory(dirname(__file__)."/template");
+        }
+        $template = $this->template_factory->open($template_file_name);
+        if ($layout) {
+            $template->set_layout($GLOBALS['template_factory']->open($layout === "without_infobox" ? 'layouts/base_without_infobox' : 'layouts/base'));
+        }
+        return $template;
+    }
+
+}
---
+++ template/copy.php
@@ -0,0 +1,17 @@
+<h2>Wiki Kopieren</h2>
+    <p>Hier haben Sie die möglichkeit das komplette Wiki in eine ihrer anderen Vorlesungen zu kopieren. Beachten Sie dabei das Wiki in der Ziel Vorlesung gelöscht wird</p>
+    <p>Infos über das Wiki dieser Vorlesungen:
+        <div>Anzahl der Wiki Seiten: <?= $infos["seiten"] ?></div>
+        <div>Letzte Änderung: <?= date("d.m.Y - G:i:s" ,$infos["aenderung"]) ?> </div>
+    </p>
+        <form action="copy">
+            <div><p>Liste der Vorlesungen</p></div>
+            <div style="width: 100%; float: left;"><div style="width: 5%; float: left;">Auswahl</div><div style="width: 10%; float: left;">Semester</div><div style="width: 40%; float: left;">Vorlesungsname</div></div>
+            <? foreach($vls as $v) : ?>
+                <div style="width: 100%; float: left;"><div style="width: 5%; float: left;">--</div><div style="width: 10%; float: left;"><?= $v["sem_name"] ?></div><div style="width: 40%; float: left;"><?= $v["name"] ?></div></div>
+            <? endforeach ?>
+            <p>
+                Wiki dieser Vorlesung (Vorlesungsname) in die obenausgewählten Vorlesungen kopieren
+                <button>Kopieren</button>
+            </p>
+        </form>
