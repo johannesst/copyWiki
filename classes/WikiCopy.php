b7ff0727519948c31dbf3f50b4531c9136bbcdab
johannes.stichler <johannes.stichler@d56861a9-e89f-4f46-9006-0833ef3c6828>
2012-12-12 14:39:00 +0000

    Start mit Version 0.5 oder wer hat den scheiß Programmiert
    git-svn-id: http://sv-svn1.hfwu.int/svn/neocopyWiki@5 d56861a9-e89f-4f46-9006-0833ef3c6828

---
+++ classes/WikiCopy.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of copyWiki
+ *
+ * @author johannesstichler
+ */
+class copyWiki {
+    //put your code here
+}
+
+?>
--- template/copy.php
+++ template/copy.php
@@ -1,31 +1,29 @@
-<h2>Wiki Kopieren</h2>
-    <p>Hier haben Sie die möglichkeit das komplette dieser Vorlesung ( <?= $vorlesungsname ?> ) Wiki in eine ihrer anderen Vorlesungen zu kopieren. Beachten Sie dabei das Wiki in der Ziel Vorlesung gelöscht wird</p>
-    <p>Infos über das Wiki dieser Vorlesungen:
-        <div>Anzahl der Wiki Seiten: <?= $infos["seiten"] ?></div>
-        <div>Letzte Änderung: <?= date("d.m.Y - G:i:s" ,$infos["aenderung"]) ?> </div>
-    </p>
+<h2>Wiki kopieren</h2>
+<div>Hier haben Sie die M&ouml;glichkeit, den kompletten Inhalt des Wikis der (Quell-)Veranstaltung <strong> <?= $vorlesungsname ?> </strong>  in das Wiki einer anderen (Ziel-)Veranstaltung zu kopieren. Bitte beachten Sie, dass Inhalte eines bereits bestehenden Wikis in der Zielveranstaltung damit gel&ouml;scht bzw. &uuml;berschrieben werden.</div>
+<div>Infos ?ber das Wiki dieser Veranstaltung:
+    <div>Anzahl der Wiki Seiten: <?= $infos["seiten"] ?></div>
+    <div>Letzte &Auml;nderung: <?= date("d.m.Y - G:i:s" ,$infos["aenderung"]) ?> </div>
+</div>
+<div>
+<div><p>Liste der Veranstaltungen</p></div>
+<div style="width: 100%; float: left;"><div style="width: 5%; float: left;">Auswahl</div><div style="width: 10%; float: left;">Semester</div><div style="width: 40%; float: left;">Veranstaltungsname</div></div>
+<? foreach($vls as $v) : ?>
+<div style="width: 100%; float: left;"><div style="width: 5%; float: left;"><input type="checkbox" name="vorlesung" value="<?= $v["id"] ?>" ></div><div style="width: 10%; float: left;"><?= $v["sem_name"] ?></div><div style="width: 40%; float: left;"><?= $v["name"] ?></div></div><br>
+<? endforeach ?>
+</div>
+<div><br>Wiki dieser Veranstaltung <strong>  <?= $vorlesungsname ?> </strong> in die oben ausgew&auml;hlten Veranstaltungen kopieren</div>
+<div><input type="checkbox" name="vorlesung" id="itsoktodelete" value="itsoktodelete">Ich bin mir bewusst, dass durch Kopieren des Wiki-Inhalts dieser Veranstaltung eventuell vorhandene Wikis in den von mir oben ausgew&auml;hlten Zielveranstaltungen unwiderruflich gel&ouml;scht bzw.&uuml;berschrieben werden. <a  href='#' onclick="helpDelete();">Bei Fragen klicken Sie hier</a></div>
+<div><button id="buttonCopyWiki">Kopieren</button></div>
 
-            <div><p>Liste der Vorlesungen</p></div>
-            <div style="width: 100%; float: left;"><div style="width: 5%; float: left;">Auswahl</div><div style="width: 10%; float: left;">Semester</div><div style="width: 40%; float: left;">Vorlesungsname</div></div>
-            <? foreach($vls as $v) : ?>
-                <div style="width: 100%; float: left;"><div style="width: 5%; float: left;"><input type="checkbox" name="vorlesung" value="<?= $v["id"] ?>" ></div><div style="width: 10%; float: left;"><?= $v["sem_name"] ?></div><div style="width: 40%; float: left;"><?= $v["name"] ?></div></div>
-            <? endforeach ?>
-            <p>
-                <p>Wiki dieser Vorlesung (Vorlesungsname) in die oben ausgew&auml;hlten Vorlesungen kopieren</p>
-                <p><input type="checkbox" name="vorlesung" id="itsoktodelete" value="itsoktodelete">Ich bestätige das durch das kopieren des Wikis, die Wikis in den Ziel Vorlesungen unwiderruflich gelöscht werden. <a onclick="helpDelete();">Bei Fragen klicken Sie hier</a></p>
-                <button id="buttonCopyWiki">Kopieren</button>
-            </p>
-            <div id="copystatus" style="display: none;">
-                Status des Kopier Vorgangs:
-                <div id="progressbar"></div>
+<div id="copystatus" style="display: none;">
+    Das Kopieren war erfolgreich
+</div>
 
-            </div>
-
-            <input id="cid" style="display: none;" value="<?= $cid ?>">
+<input id="cid" style="display: none;" value="<?= $cid ?>">
 
 
 <div id="copyHelpDelete" style="display: none;">
     <h3>Hilfe</h3>
-    <p>Mit dem Tool copyWiki haben Sie die Möglichkeit von einer Vorlesung das Wiki in mehrere Vorlesungen zu kopieren. Aus technischen Gründen wird das Wiki in der Ziel Vorlesung gelöscht</p>
+    <p>Mit dem Tool copyWiki haben Sie die M?glichkeit von einer Veranstaltung das Wiki in mehrere Veranstaltungen zu kopieren. Aus technischen Gr?nden wird das Wiki in der Ziel Veranstaltung gel&ouml;scht</p>
 
 </div>
